991d9b689bffd020951ffa4f1f5766e58e62738d
Kristen Groller <groller@rogers.com>
2021-02-23 19:16:14 -0500

    add post route-all working now

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const sqlite3 = require('sqlite3').verbose();
 const express = require('express');
 
+const inputCheck = require('./utils/inputCheck');
+
 const PORT = process.env.PORT || 3001;
 const app = express();
 
@@ -55,7 +57,7 @@ app.get('/api/candidates/:id', (req, res) => {
     
 
 //Deleta a candidate
-app.delete('/api/candidate/:id', (req, res) => {
+app.delete('/api/candidates/:id', (req, res) => {
     const sql = `DELETE FROM candidates WHERE id = ?`;
     const params = [req.params.id];
     db.run(sql, params, function(err, result) {
@@ -71,20 +73,31 @@ app.delete('/api/candidate/:id', (req, res) => {
     });
   });
 
-
 //Create a candidate
-/*const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-            VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
+app.post('/api/candidates', ({ body}, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', "industry_connected");
+    if (errors) {
+        res.status(400).json({ errors: errors });
+        return;
+    }
+
+const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+            VALUES (?,?,?)`;
+const params = [body.first_name, body.last_name, body.industry_connected];
 
 //ES5 function, not arrow function, to use this
 db.run(sql, params, function(err, result) {
     if(err) {
-        console.log(err);
-    }
-    console.log(result, this.lastID);
-});*/
-
+      res.status(400).json({ error: err.message });
+      return;    
+}
+res.json({
+    message: 'success',
+    data: body,
+    id: this.lastID
+});
+});
+});
 //Default reponse for any other request (Not Found) Catch all
 app.use((req, res) => {
     res.status(404).end();
